a0fd371820e257af9379af08600f307df5d57e63
Smruti <lenkasmruti123@gmail.com>
2021-11-25 22:46:13 +0530

    Added:CaseStatement

---
+++ CaseStatement/readnumber.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+let number = prompt("Enter a number");
+switch(number){
+    case "1" :
+        console.log("Unit");
+        break;
+    case "10" :
+        console.log("Ten");
+        break;
+    case "100" :
+        console.log("Hundred");
+        break;
+    case "1000" :
+        console.log("Thousand");
+        break;
+    default :
+        console.log("Invalid Input");
+}
