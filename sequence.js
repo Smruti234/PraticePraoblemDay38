9f8eef15d6448a384249adf547626e5386ba28a5
Smruti <lenkasmruti123@gmail.com>
2021-11-25 17:44:44 +0530

    Added:N4_sequence

---
+++ sequence.js
@@ -0,0 +1,9 @@
+let a = Math.floor(Math.random() * 80 + 10); 
+let b = Math.floor(Math.random() * 80 + 10); 
+let c = Math.floor(Math.random() * 80 + 10);
+let d = Math.floor(Math.random() * 80 + 10);
+let e = Math.floor(Math.random() * 80 + 10);
+let sum = a+b+c+d+e;
+let avg = sum/5;
+console.log("Sum of the five number :" + sum);
+console.log("Average of the five number :" + avg);
